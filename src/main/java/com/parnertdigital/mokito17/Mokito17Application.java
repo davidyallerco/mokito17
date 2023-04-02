e8a3eb88de448365df6e03acf4d1c8c5cd93eda9
davidyallerco <david.yallerco@gmail.com>
2023-04-02 11:43:32 -0500

    primer comit

---
+++ src/main/java/com/parnertdigital/mokito17/Mokito17Application.java
@@ -0,0 +1,13 @@
+package com.parnertdigital.mokito17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mokito17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mokito17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/parnertdigital/mokito17/Mokito17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parnertdigital.mokito17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mokito17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
